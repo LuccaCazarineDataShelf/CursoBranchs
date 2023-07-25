683b318de00a82c8a73d4a33f3903bcb04013136
lucca <luccacazarine@gmail.com>
2023-07-25 17:03:12 -0300

    commit 1

---
+++ ProjetoAula08/Pessoa.java
@@ -0,0 +1,28 @@
+public class Pessoa{
+        public String nome;
+        public String sexo;
+        public int idade;
+
+        public String getNome(){
+            return this.getNome();
+        }
+        public void setNome(String n){
+            this.nome = n;
+        }
+        public String getSexo(){
+            return this.sexo;
+        }
+        public void setSexo(String s){
+            this.sexo = s;
+        }
+        public int getIdade(){
+            return this.idade;
+        }
+        public void setIdade(int i){
+            this.idade = i;
+        }
+
+        public void fazerAniversario(){
+
+        }
+}
